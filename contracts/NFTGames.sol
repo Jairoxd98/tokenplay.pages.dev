566a8ec1132921d69cbccd215c94c90e54a8a2b8
Joan Jimenez Jané <jimenezxik91@gmail.com>
2023-08-18 17:51:33 +0200

    Añadir funciones para devolver los juegos por categorias, añadir categorias a los juegos, añadir royalty por juego en vez de genérico, añadir el timestamp para saber cuando un juego debe estar disponible para ser comprado

--- contracts/NFTGames.sol
+++ contracts/NFTGames.sol
@@ -11,16 +11,18 @@ contract NFTGames is ERC1155, ERC1155URIStorage, Ownable {
     using SafeMath for uint256;
     using EnumerableSet for EnumerableSet.UintSet;
 
-    uint256 public royaltyPercentage = 10; // % royalties
     bool public mintIsActive = false;
 
     // Estructura para guardar los datos relevantes del NFT
     struct NftGameInfo {
         string name;
         string description;
+        string category;
         string imageUrl;
         uint256 tokenId;
         uint256 price;
+        uint256 gameRelease;
+        uint256 tokenPlayRoyaltyPercentage;
         address gameOwnerAddress;
     }
 
@@ -40,11 +42,11 @@ contract NFTGames is ERC1155, ERC1155URIStorage, Ownable {
     mapping(address => EnumerableSet.UintSet) private purchasedNFTs;
 
     // Función para añadir un juego (crear un token)
-    function addNFT(string memory name, string memory description, string memory imageUrl, uint256 price, address gameOwnerAddress) external onlyOwner {
+    function addNFT(string memory name, string memory description, string memory category, string memory imageUrl, uint256 price, uint256 gameRelease, uint256 tokenPlayRoyaltyPercentage, address gameOwnerAddress) external onlyOwner {
         uint256 tokenId = nextTokenId;
         nextTokenId = nextTokenId.add(1);
 
-        gamesInfo[tokenId] = NftGameInfo(name, description, imageUrl, tokenId, price, gameOwnerAddress);
+        gamesInfo[tokenId] = NftGameInfo(name, description, category, imageUrl, tokenId, price, gameRelease, tokenPlayRoyaltyPercentage, gameOwnerAddress);
     }
 
     // Función para comprar un juego (un token)
@@ -62,7 +64,7 @@ contract NFTGames is ERC1155, ERC1155URIStorage, Ownable {
 
         // Calculamos los porcentajes que se lleva cada parte y hacemos la transferencia de los tokens
         uint256 paymentAmount = price;
-        uint256 tokenPlay = paymentAmount.mul(royaltyPercentage).div(100); // royaltypercentage para token play
+        uint256 tokenPlay = paymentAmount.mul(gamesInfo[tokenId].tokenPlayRoyaltyPercentage).div(100); // royaltypercentage para token play
         uint256 gameOwner = paymentAmount.sub(tokenPlay);  // el restante para game owner
 
         payable(owner()).transfer(tokenPlay);
@@ -76,6 +78,11 @@ contract NFTGames is ERC1155, ERC1155URIStorage, Ownable {
 
     // Funcion para obtener los juegos que ha comprado un usuario
     function getPurchasedNFTs(address account) external view returns (PurchasedNft[] memory) {
+        require(account != address(0), "Invalid address");
+
+        uint256 purchasedCount = purchasedNFTs[account].length();
+        require(purchasedCount > 0, "The user has not purchased any NFTs");
+
         PurchasedNft[] memory accountGames = new PurchasedNft[](purchasedNFTs[account].length());
 
         for (uint256 i = 0; i < purchasedNFTs[account].length(); i++) {
@@ -99,14 +106,42 @@ contract NFTGames is ERC1155, ERC1155URIStorage, Ownable {
         return allNFTs;
     }
 
+    // Función para obtener los juegos de una categoria
+    function getCategoryNFTs(string memory category) external view returns (NftGameInfo[] memory) {
+        NftGameInfo[] memory matchingNFTs;
+        uint256 matchingCount = 0;
+
+        for (uint256 i = 0; i < nextTokenId; i++) {
+            NftGameInfo storage nftInfo = gamesInfo[i];
+            if(keccak256(abi.encodePacked(nftInfo.category)) == keccak256(abi.encodePacked(category))){
+                matchingCount++;
+            }
+        }
+
+        matchingNFTs = new NftGameInfo[](matchingCount);
+        uint256 currentIndex = 0;
+
+        for (uint256 i = 0; i < nextTokenId; i++) {
+            NftGameInfo storage nftInfo = gamesInfo[i];
+            if(keccak256(abi.encodePacked(nftInfo.category)) == keccak256(abi.encodePacked(category))){
+                matchingNFTs[currentIndex] = nftInfo;
+                currentIndex++;
+            }
+        }
+
+        return matchingNFTs;
+    }
+
     // Función para que el propietario retire los fondos acumulados
     function withdrawBalance() external onlyOwner {
         payable(owner()).transfer(address(this).balance);
     }
 
     // Función para cambiar el porcentaje de royalty que nos llevamos
-    function setRoyaltyPercentage(uint256 percentage) public onlyOwner {
-        royaltyPercentage = percentage;
+    function setRoyaltyPercentage(uint256 tokenId, uint256 royaltyPercentage) public onlyOwner {
+        require(tokenId >= 0 && tokenId < nextTokenId, "Invalid token ID");
+
+        gamesInfo[tokenId].tokenPlayRoyaltyPercentage = royaltyPercentage;
     }
 
     // Función para activar o desactivar la compra de juegos (tokens)
