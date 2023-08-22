4d3e017ba586f5ae1f4ae0f0ee15cda27f5cbde0
Joan Jimenez Jané <jimenezxik91@gmail.com>
2023-08-22 16:31:59 +0200

    Corregir errores del SC y añadir función para obtener los juegos en venta

--- contracts/NFTGamesMarketplace.sol
+++ contracts/NFTGamesMarketplace.sol
@@ -19,8 +19,12 @@ contract NFTGamesMarketplace is Ownable, ERC1155Holder  {
         nftContract = NFTGames(_nftContract);
     }
 
+    // Contador autoincremental, cada número será una venta
+    uint256 public nextSaleId = 0;
+
     // Estructura para poner a la venta un juego
     struct Sale {
+        uint256 saleId;
         uint256 tokenId;
         address seller;
         uint256 price;
@@ -38,26 +42,36 @@ contract NFTGamesMarketplace is Ownable, ERC1155Holder  {
 
         nftContract.safeTransferFrom(msg.sender, address(this), tokenId, 1, "");
 
-        Sale memory sale = Sale({tokenId: tokenId, seller: msg.sender, price: price, arrayPosition: sales.length, canceled: false, selled: false});
+        uint256 saleId = nextSaleId;
+        nextSaleId = nextSaleId.add(1);
+
+        Sale memory sale = Sale({saleId: saleId, tokenId: tokenId, seller: msg.sender, price: price, arrayPosition: sales.length, canceled: false, selled: false});
         sales.push(sale);
-        salesInfo[tokenId] = sale;
+        salesInfo[saleId] = sale;
     }
 
-    function cancelSale(uint256 tokenId) external {
-        require(tokenId >= 0, "Invalid token id");
+    function cancelSale(uint256 saleId) payable external {
+        require(saleId >= 0, "Invalid sale id");
 
-        Sale memory saleMapping = salesInfo[tokenId];
+        Sale storage saleMapping = salesInfo[saleId];
         require(saleMapping.seller == msg.sender, "You are not the seller");
         require(saleMapping.canceled == false, "The sale has already been canceled");
+        require(saleMapping.selled == false, "The sale has been sold");
         require(saleMapping.arrayPosition < sales.length, "Strange error, contact with administrators (1)");
 
         Sale memory saleArray = sales[saleMapping.arrayPosition];
-        require(saleMapping.tokenId == saleArray.tokenId, "Strange error, contact with administrators (2)");
+        require(saleMapping.saleId == saleArray.saleId, "Strange error, contact with administrators (2)");
 
         saleMapping.canceled = true;
         // Reorganizar el array moviendo el último elemento al lugar del elemento eliminado
         if (saleMapping.arrayPosition < sales.length - 1) {
             Sale storage lastSale = sales[sales.length - 1];
+
+            // Actualizamos posiciones
+            lastSale.arrayPosition = saleMapping.arrayPosition;
+            salesInfo[lastSale.saleId].arrayPosition = saleMapping.arrayPosition;
+
+            // Guardamos elemento en la nueva posición del array
             sales[saleMapping.arrayPosition] = lastSale;
         }
         sales.pop();
@@ -65,20 +79,28 @@ contract NFTGamesMarketplace is Ownable, ERC1155Holder  {
         nftContract.safeTransferFrom(address(this), msg.sender, saleMapping.tokenId, 1, "");
     }
 
-    function purchaseFromMarketplace(uint256 tokenId) external payable {
-        require(tokenId >= 0, "Invalid token id");
+    function purchaseFromMarketplace(uint256 saleId) external payable {
+        require(saleId >= 0, "Ivalid sale id");
 
-        Sale storage saleMapping = salesInfo[tokenId];
+        Sale storage saleMapping = salesInfo[saleId];
         require(saleMapping.canceled == false, "The sale is canceled");
+        require(saleMapping.selled == false, "The sale is selled");
         require(msg.value >= saleMapping.price, "Insufficient funds");
         require(saleMapping.arrayPosition < sales.length, "Strange error, contact with administrators (1)");
 
         Sale memory saleArray = sales[saleMapping.arrayPosition];
-        require(saleMapping.tokenId == saleArray.tokenId, "Strange error, contact with administrators (2)");
+        require(saleMapping.saleId == saleArray.saleId, "Strange error, contact with administrators (2)");
 
         // Reorganizar el array moviendo el último elemento al lugar del elemento eliminado
+        saleMapping.selled = true;
         if (saleMapping.arrayPosition < sales.length - 1) {
             Sale storage lastSale = sales[sales.length - 1];
+
+            // Actualizamos posiciones
+            lastSale.arrayPosition = saleMapping.arrayPosition;
+            salesInfo[lastSale.saleId].arrayPosition = saleMapping.arrayPosition;
+
+            // Guardamos elemento en la nueva posición del array
             sales[saleMapping.arrayPosition] = lastSale;
         }
         sales.pop();
@@ -88,5 +110,31 @@ contract NFTGamesMarketplace is Ownable, ERC1155Holder  {
         if (msg.value > saleMapping.price) {
             payable(msg.sender).transfer(msg.value - saleMapping.price);
         }
+    }
+
+    // Función para obtener los juegos en venta
+    function getGamesForSale() external view returns (Sale[] memory) {
+        uint256 activeSalesCount = 0;
+
+        // Contar la cantidad de ventas activas
+        for (uint256 i = 0; i < sales.length; i++) {
+            if (!sales[i].canceled && !sales[i].selled) {
+                activeSalesCount++;
+            }
+        }
+
+        // Crear un array para almacenar las ventas activas
+        Sale[] memory activeSales = new Sale[](activeSalesCount);
+
+        // Llenar el array con las ventas activas
+        uint256 activeIndex = 0;
+        for (uint256 i = 0; i < sales.length; i++) {
+            if (!sales[i].canceled && !sales[i].selled) {
+                activeSales[activeIndex] = sales[i];
+                activeIndex++;
+            }
+        }
+
+        return activeSales;
     }
 }
