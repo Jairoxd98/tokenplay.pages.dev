0d7ea663bd786ff2d77680fbacde666445062b55
Jairoxd98 <Jairo.Celada@alu.uclm.es>
2023-08-30 19:07:47 +0200

    setup update

--- setup.js
+++ setup.js
@@ -15,11 +15,9 @@ module.exports = async function (callback) {
     let priceNFT = web3.utils.toBN("1000000000000000000");
     //console.log(priceNFT);
 
-    await _TOKENPLAY.addNFT(10007,priceNFT,accounts[3],5,15,0, {from: accounts[0]});
+    await _TOKENPLAY.addNFT(10006,priceNFT,accounts[3],5,15,0, {from: accounts[0]});
 
-    await _TOKENPLAY.purchaseNFT(10007, {from: accounts[4], value: 2000000000000000000 });
-
-    console.log(await _TOKENPLAY.totalSupply(10007));
+    await _TOKENPLAY.purchaseNFT(10006, {from: accounts[4], value: 2000000000000000000 });
 
     // await _TOKENPLAY.flipMintState();
 
--- setup2.js
+++ setup2.js
@@ -8,33 +8,35 @@ module.exports = async function (callback) {
   var tokenplayInstance = await TOKENPLAY.deployed();
 
   // Deploy NFTGamesMarketplace
-  //const marketplaceInstance = await NFTGamesMarketplace.new(tokenplayInstance.address, { from: accounts[0] });
   var marketplaceInstance = await NFTGamesMarketplace.deployed();
 
-  // Aprobación del marketplace en el contrato TOKENPLAY
   const seller = accounts[4];
+  const buyer = accounts[1];
+  // Se da de alta en el market
   await tokenplayInstance.approveMarketplace(marketplaceInstance.address, { from: seller });
 
-  // Creación de una venta en el contrato NFTGamesMarketplace
   const price = web3.utils.toWei("1", "ether");
-  console.log(price);
   try{
-    await marketplaceInstance.createSale(10002, price, { from: seller });
-    console.log(true); // Peta aquí
+    // Crea anuncio
+    await marketplaceInstance.createSale(10007, price, { from: seller });
+    console.log(await marketplaceInstance.getGamesForSale());
+
+    // Cancela anuncio
+    await marketplaceInstance.cancelSale(0, { from: seller });
+    console.log(await marketplaceInstance.getGamesForSale());
+
+    // Crea anuncio
+    await marketplaceInstance.createSale(10007, price, { from: seller });
+    console.log(await marketplaceInstance.getGamesForSale());
+
+    // Compra anuncio
+    await marketplaceInstance.purchaseFromMarketplace(1, { from: buyer, value: price });
+    console.log(await marketplaceInstance.getGamesForSale());
+
   }
   catch (error) {
     console.error("Error:", error);
   }
-
-  // Obtener información sobre los juegos en venta y realizar algunas comprobaciones
-  const gamesForSale = await marketplaceInstance.getGamesForSale();
-  console.log(gamesForSale);
-  console.log(true);
-
-  const sale = await marketplaceInstance.salesInfo(0);
-  console.log(sale);
-  const sale1 = await marketplaceInstance.salesInfo(1);
-  console.log(sale1);
 
   callback();
 };
