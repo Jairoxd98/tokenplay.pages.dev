8d20536fe570000c874ecc3e6ef9bbb494925cdb
Joan Jimenez Jané <jimenezxik91@gmail.com>
2023-08-30 16:27:49 +0200

    Arreglar los SETUP.js para que funcione bien el hecho de crear la venta y no se quede pillado todo el rato

--- setup2.js
+++ setup2.js
@@ -12,14 +12,19 @@ module.exports = async function (callback) {
   var marketplaceInstance = await NFTGamesMarketplace.deployed();
 
   // Aprobación del marketplace en el contrato TOKENPLAY
-  const seller = accounts[2];
+  const seller = accounts[4];
   await tokenplayInstance.approveMarketplace(marketplaceInstance.address, { from: seller });
 
   // Creación de una venta en el contrato NFTGamesMarketplace
   const price = web3.utils.toWei("1", "ether");
   console.log(price);
-  await marketplaceInstance.createSale(10002, price, { from: seller });
-  console.log(true); // Peta aquí
+  try{
+    await marketplaceInstance.createSale(10002, price, { from: seller });
+    console.log(true); // Peta aquí
+  }
+  catch (error) {
+    console.error("Error:", error);
+  }
 
   // Obtener información sobre los juegos en venta y realizar algunas comprobaciones
   const gamesForSale = await marketplaceInstance.getGamesForSale();
